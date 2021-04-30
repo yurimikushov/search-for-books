786673ab776c4cdc48fa8c6d0f177ed79223e338
Mikushov Yuri <mikushov.yu@gmail.com>
2021-04-30 08:52:19 +0300

    refactor: corrected code style

--- src/store/reducers/suggests.js
+++ src/store/reducers/suggests.js
@@ -14,20 +14,17 @@ const suggestsReducer = (state = initailState, { type, payload }) => {
   switch (type) {
     case FETCH_SUGGESTS_LOADING:
       return {
+        ...initailState,
         isLoading: true,
-        suggests: [],
-        error: '',
       }
     case FETCH_SUGGESTS_SUCCESS:
       return {
-        isLoading: false,
+        ...initailState,
         suggests: payload.suggests,
-        error: '',
       }
     case FETCH_SUGGESTS_ERROR:
       return {
-        isLoading: false,
-        suggests: [],
+        ...initailState,
         error: payload.errorMessage,
       }
     default:
