f4c62039f6292083c942a4f2bba28ffb2fcc2d65
Mikushov Yuri <mikushov.yu@gmail.com>
2021-05-01 11:37:41 +0300

    feat: styled book layout

--- src/components/Book/index.jsx
+++ src/components/Book/index.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import './index.css'
 
-const Book = ({ title, author, publicationDate, publisher, isbn }) => (
+const Book = ({ img, title, author, publishDate, publisher, isbn }) => (
   <div className='book'>
-    <div className='book__cover'></div>
-    <div className='book__title'>{title}</div>
-    <div className='book__author'>{author}</div>
-    <div className='book__publication-date'>{publicationDate}</div>
-    <div className='book__publisher'>{publisher}</div>
-    <div className='book__isbn'>{isbn}</div>
-    <div>
-      {JSON.stringify(
-        { title, author, publicationDate, publisher, isbn },
-        null,
-        2
-      )}
+    <div className='book__cover'>
+      {img && <img className='book__cover-img' src={img} />}
+    </div>
+    <div className='book__info'>
+      <div className='book__title'>{title}</div>
+      <div className='book__author'>{author}</div>
+      <div className='book__isbn'>ISBN: {isbn}</div>
+      <div className='book__publication-info'>
+        <span className='book__publisher'>{publisher}, </span>
+        <span className='book__published'>{publishDate}</span>
+      </div>
     </div>
   </div>
 )
