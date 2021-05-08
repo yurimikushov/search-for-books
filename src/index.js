d00a9878d62cacfab8f9bb1da12d9f3c2c7af09e
Mikushov Yuri <mikushov.yu@gmail.com>
2021-05-08 09:36:37 +0300

    chore: wrapped state init with strict mode

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from './App'
 import './index.css'
 
 render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('.main')
 )
