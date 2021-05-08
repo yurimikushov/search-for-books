f641f1438cc6f1c6ff1e51e54306699137b6fa21
Mikushov Yuri <mikushov.yu@gmail.com>
2021-05-08 21:45:02 +0300

    feat: added suggests fetching after suggests choosing

--- src/hooks/useChooseSuggest.js
+++ src/hooks/useChooseSuggest.js
@@ -1,13 +1,15 @@
 import { useSearchQuery } from '../store/hooks'
-import { useSearchBooks } from '../hooks'
+import { useSearchBooks, useFetchSuggests } from '../hooks'
 
 const useChooseSuggest = () => {
   const [_, setSearchQuery] = useSearchQuery()
   const searchBooks = useSearchBooks()
+  const fetchSuggests = useFetchSuggests()
 
   return (title) => {
     setSearchQuery(title)
     searchBooks(title)
+    fetchSuggests(title)
   }
 }
 
