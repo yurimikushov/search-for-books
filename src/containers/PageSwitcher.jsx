7e5e82e7c295ce095d47a32e076c29ee57c76be0
Mikushov Yuri <mikushov.yu@gmail.com>
2021-05-13 08:53:22 +0300

    feat: added auto calc of page list for page switcher

--- src/containers/PageSwitcher.jsx
+++ src/containers/PageSwitcher.jsx
@@ -1,44 +1,23 @@
 import React from 'react'
+import { useSearchBooks } from '../hooks'
+import { useSearchQuery, useFoundBooks } from '../store/hooks'
+import { createPageList } from '../utils'
 import PageSwitcher from '../components/PageSwitcher'
 
 const PageSwitcherContainer = () => {
-  const pages = [
-    {
-      type: 'begin',
-      title: 'В начало',
-      isCurrentPage: false,
-    },
-    {
-      type: 'page',
-      title: '1',
-      isCurrentPage: false,
-    },
-    {
-      type: 'page',
-      title: '2',
-      isCurrentPage: true,
-    },
-    {
-      type: 'page',
-      title: '3',
-      isCurrentPage: false,
-    },
-    {
-      type: 'page',
-      title: '4',
-      isCurrentPage: false,
-    },
-    {
-      type: 'page',
-      title: '5',
-      isCurrentPage: false,
-    },
-    {
-      type: 'next',
-      title: 'дальше',
-      isCurrentPage: false,
-    },
-  ]
+  const onSearch = useSearchBooks()
+  const [searchQuery] = useSearchQuery()
+  const { isLoading, currentPage, numPages } = useFoundBooks()
+
+  if (isLoading) {
+    return null
+  }
+
+  const pages = createPageList(currentPage, numPages)
+
+  if (pages.length === 0) {
+    return null
+  }
 
   const onSwitchHandler = (page) => {
     onSearch(searchQuery, page)
---
+++ src/utils/createPageList.js
@@ -0,0 +1,61 @@
+const MAX_NUM_DISPLAYED_ITEMS = 5
+
+const createBegin = (title) => ({
+  type: 'begin',
+  title,
+  pageNum: 1,
+})
+
+const createPage = (pageNum, isCurrentPage) => ({
+  type: 'page',
+  title: pageNum.toString(),
+  pageNum: pageNum,
+  isCurrentPage,
+})
+
+const createNext = (title, pageNum) => ({
+  type: 'next',
+  title,
+  pageNum,
+})
+
+const createPageList = (currentPageNum, numPages) => {
+  if (numPages <= 0) {
+    return []
+  }
+
+  const pageNumList = []
+
+  if (1 < currentPageNum) {
+    pageNumList.push(createBegin('В начало'))
+  }
+
+  const NUM_DISPLAYED_ITEMS =
+    numPages < MAX_NUM_DISPLAYED_ITEMS ? numPages : MAX_NUM_DISPLAYED_ITEMS
+
+  if (currentPageNum < Math.ceil(NUM_DISPLAYED_ITEMS / 2)) {
+    for (let pageNum = 1; pageNum <= NUM_DISPLAYED_ITEMS; pageNum++) {
+      pageNumList.push(createPage(pageNum, pageNum === currentPageNum))
+    }
+  } else {
+    const leftSidePageNum =
+      NUM_DISPLAYED_ITEMS / 2 === Math.round(NUM_DISPLAYED_ITEMS / 2)
+        ? NUM_DISPLAYED_ITEMS / 2 - 1
+        : Math.floor(NUM_DISPLAYED_ITEMS / 2)
+
+    const firstPageNum = currentPageNum - leftSidePageNum
+    const lastPageNum = firstPageNum + NUM_DISPLAYED_ITEMS - 1
+
+    for (let pageNum = firstPageNum; pageNum <= lastPageNum; pageNum++) {
+      pageNumList.push(createPage(pageNum, pageNum === currentPageNum))
+    }
+  }
+
+  if (currentPageNum < numPages) {
+    pageNumList.push(createNext('дальше', currentPageNum + 1))
+  }
+
+  return pageNumList
+}
+
+export { createPageList }
