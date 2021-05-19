6b489a2a49fb19747d141e16527ba5095905657b
Mikushov Yuri <mikushov.yu@gmail.com>
2021-05-19 09:11:29 +0300

    fix: setted z-index for popups

--- src/components/SuggestsPopup/index.jsx
+++ src/components/SuggestsPopup/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { createPortal } from 'react-dom'
 import PropTypes from 'prop-types'
+import './index.css'
 
 const SuggestsPopup = ({ children, top, left, width }) =>
   createPortal(
     <div
       className='suggests-popup__content'
-      style={{ position: 'absolute', top, left, width }}
+      style={{
+        top,
+        left,
+        width,
+      }}
     >
       {children}
     </div>,
