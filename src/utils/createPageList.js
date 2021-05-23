27affdb6bbcb95f751d6de42f484e4d7c2b5777a
Mikushov Yuri <mikushov.yu@gmail.com>
2021-05-23 10:31:05 +0300

    fix: corrected calculation of start and end items of page switcher

--- src/utils/createPageList.js
+++ src/utils/createPageList.js
@@ -1,8 +1,8 @@
 const MAX_NUM_DISPLAYED_ITEMS = 5
 
-const createBegin = (title) => ({
+const createBegin = () => ({
   type: 'begin',
-  title,
+  title: 'В начало',
   pageNum: 1,
 })
 
@@ -13,49 +13,50 @@ const createPage = (pageNum, isCurrentPage) => ({
   isCurrentPage,
 })
 
-const createNext = (title, pageNum) => ({
+const createNext = (nextPageNum) => ({
   type: 'next',
-  title,
-  pageNum,
+  title: 'дальше',
+  pageNum: nextPageNum,
 })
 
+const calcStartAndEndPages = (currentPageNum, numPages) => {
+  const NUM_DISPLAYED_ITEMS =
+    numPages < MAX_NUM_DISPLAYED_ITEMS ? numPages : MAX_NUM_DISPLAYED_ITEMS
+
+  if (currentPageNum < Math.ceil(NUM_DISPLAYED_ITEMS / 2)) {
+    return [1, NUM_DISPLAYED_ITEMS]
+  }
+
+  const pageNumOnRightSide =
+    NUM_DISPLAYED_ITEMS / 2 === Math.round(NUM_DISPLAYED_ITEMS / 2)
+      ? NUM_DISPLAYED_ITEMS / 2 - 1
+      : Math.floor(NUM_DISPLAYED_ITEMS / 2)
+
+  const end = Math.min(currentPageNum + pageNumOnRightSide, numPages)
+  const start = Math.max(end - NUM_DISPLAYED_ITEMS + 1, 1)
+
+  return [start, end]
+}
+
 const createPageList = (currentPageNum, numPages) => {
-  if (numPages <= 0) {
+  if (numPages < 1) {
     return []
   }
 
   const pageNumList = []
 
   if (1 < currentPageNum) {
-    pageNumList.push(createBegin('В начало'))
+    pageNumList.push(createBegin())
   }
 
-  const NUM_DISPLAYED_ITEMS =
-    numPages < MAX_NUM_DISPLAYED_ITEMS ? numPages : MAX_NUM_DISPLAYED_ITEMS
+  const [start, end] = calcStartAndEndPages(currentPageNum, numPages)
 
-  if (currentPageNum < Math.ceil(NUM_DISPLAYED_ITEMS / 2)) {
-    for (let pageNum = 1; pageNum <= NUM_DISPLAYED_ITEMS; pageNum++) {
-      pageNumList.push(createPage(pageNum, pageNum === currentPageNum))
-    }
-  } else {
-    const leftSidePageNum =
-      NUM_DISPLAYED_ITEMS / 2 === Math.round(NUM_DISPLAYED_ITEMS / 2)
-        ? NUM_DISPLAYED_ITEMS / 2 - 1
-        : Math.floor(NUM_DISPLAYED_ITEMS / 2)
-
-    const firstPageNum = currentPageNum - leftSidePageNum
-    const lastPageNum = Math.min(
-      firstPageNum + NUM_DISPLAYED_ITEMS - 1,
-      numPages
-    )
-
-    for (let pageNum = firstPageNum; pageNum <= lastPageNum; pageNum++) {
-      pageNumList.push(createPage(pageNum, pageNum === currentPageNum))
-    }
+  for (let pageNum = start; pageNum <= end; pageNum++) {
+    pageNumList.push(createPage(pageNum, pageNum === currentPageNum))
   }
 
   if (currentPageNum < numPages) {
-    pageNumList.push(createNext('дальше', currentPageNum + 1))
+    pageNumList.push(createNext(currentPageNum + 1))
   }
 
   return pageNumList
