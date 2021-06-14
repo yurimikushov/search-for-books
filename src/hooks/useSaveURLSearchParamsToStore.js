6dd3f1e7a11c44bfb79b98a2dce8e040a3599a98
Mikushov Yuri <mikushov.yu@gmail.com>
2021-06-14 16:22:50 +0300

    fix: added saving url search params to store after each change to url search params

---
+++ src/hooks/useSaveURLSearchParamsToStore.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import { useSearchQuery, useSearchPage } from 'store/hooks'
+import { useQueryParams } from 'hooks'
+
+const useSaveURLSearchParamsToStore = () => {
+  const { q, p } = useQueryParams()
+  const setSearchQuery = useSearchQuery()[1]
+  const setSearchPage = useSearchPage()[1]
+
+  useEffect(() => {
+    setSearchQuery(q)
+    setSearchPage(p)
+  }, [q, p])
+}
+
+export { useSaveURLSearchParamsToStore }
--- src/pages/FoundBooks.jsx
+++ src/pages/FoundBooks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useReconcileSearchParams, useFetchBooks } from 'hooks'
+import { useSaveURLSearchParamsToStore, useFetchBooks } from 'hooks'
 import SearchForm from 'containers/SearchForm'
 import FoundBooks from 'containers/FoundBooks'
 import PageSwitcher from 'containers/PageSwitcher'
@@ -7,7 +7,7 @@ import SuggestsPopup from 'containers/SuggestsPopup'
 import BookPopup from 'containers/BookPopup'
 
 const FoundBooksPage = () => {
-  useReconcileSearchParams()
+  useSaveURLSearchParamsToStore()
   useFetchBooks()
 
   return (
