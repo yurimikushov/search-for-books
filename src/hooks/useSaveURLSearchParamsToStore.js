f1c70afe64209ce77ec3865b69dd954a2695662a
Mikushov Yuri <mikushov.yu@gmail.com>
2021-06-15 08:58:06 +0300

    fix: setted default search params

--- src/hooks/useSaveURLSearchParamsToStore.js
+++ src/hooks/useSaveURLSearchParamsToStore.js
@@ -3,7 +3,7 @@ import { useSearchQuery, useSearchPage } from 'store/hooks'
 import { useQueryParams } from 'hooks'
 
 const useSaveURLSearchParamsToStore = () => {
-  const { q, p } = useQueryParams()
+  const { q = '', p = 0 } = useQueryParams()
   const setSearchQuery = useSearchQuery()[1]
   const setSearchPage = useSearchPage()[1]
 
